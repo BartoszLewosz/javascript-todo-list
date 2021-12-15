a92583223f83a29ceb1fa04bdbc72bc4085f5994
bartek <bartosz.lewosz@gmail.com>
2021-12-15 17:30:23 +0000

    add function for button with preventDefault()

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoContainer = document.querySelector(".todo-container");
 const todoList = document.querySelector(".todo-list");
+// selectors;
 
 // event listeners
+todoButton.addEventListener("click", addTodoItem);
+// event listeners
 
+// functions
+function addTodoItem(event) {
+  event.preventDefault();
+}
 // functions

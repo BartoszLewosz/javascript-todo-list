4c0da08bbe26b608dd2cbdd5626479c1385a001b
bartek <bartosz.lewosz@gmail.com>
2021-12-15 17:20:40 +0000

    create selector for todoInput, todoButton, todoContainer, todoList

---
+++ app.js
@@ -0,0 +1,9 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoContainer = document.querySelector(".todo-container");
+const todoList = document.querySelector(".todo-list");
+
+// event listeners
+
+// functions

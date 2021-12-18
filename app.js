49f9d0ea9cd085dc4d70817b626003add7cb1321
bartek <bartosz.lewosz@gmail.com>
2021-12-18 19:33:43 +0000

    create case statemnt for checking if 'todo-completed' class appears

--- app.js
+++ app.js
@@ -58,5 +58,26 @@ function deleteTodoItem(event) {
 
 function todoFilterOption(e) {
   const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("todo-completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("todo-completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
 // functions
